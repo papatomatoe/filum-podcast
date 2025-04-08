5adf0027699ec9d84961d138dd0de3d2a561b1ba
papatomatoe <papatomatoe.project@gmail.com>
2025-04-08 10:18:39 +0300

    test

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,7 +5,7 @@ export const prerender = true;
 export const load: PageLoad = async ({ fetch }) => {
 	const response = await fetch('/api/episodes');
 
-	const episodes = await response.json();
+	const episodes = response.ok ? await response.json() : [];
 
 	return {
 		episodes: episodes.slice(0, 3)
