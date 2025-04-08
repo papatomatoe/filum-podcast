42e99b67b61ef43b148c0a94fd917f960aa61a71
papatomatoe <papatomatoe.project@gmail.com>
2025-04-08 12:14:18 +0300

    test

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,10 @@
+import { base } from '$app/paths';
 import type { PageLoad } from './$types';
 
 export const prerender = true;
 
 export const load: PageLoad = async ({ fetch }) => {
-	const response = await fetch('/api/episodes');
+	const response = await fetch(`${base}/api/episodes`);
 
 	const episodes = response.ok ? await response.json() : [];
 
